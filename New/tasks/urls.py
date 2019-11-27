fc8ba33bc0e7b0c7d299166d6341adf5b7298f7a
Alexsandro <sandrobass@hotmail.com>
2019-11-27 01:33:53 +0000

    testes

---
+++ New/tasks/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='new-task'),
+    path('edittask/<int:id>', views.editTask, name='edit-task'),
+    path('deletetask/<int:id>', views.deleteTask, name='delete-task'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
+    path('helloworld/', views.helloworld),
+
+]
